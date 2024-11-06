a94a646a26b4d8786982fc48a79a9963a848e4cc
Lucas Merenciano <lucas.merenciano@zitrogames.com>
2024-11-06 14:02:43 +0100

    str32 hash

--- lu_fixedstr.h
+++ lu_fixedstr.h
@@ -1,6 +1,7 @@
 #ifndef __LU_FIXED_STRING_H__
 #define __LU_FIXED_STRING_H__
 
+#include <stdint.h>
 #include <string.h>
 
 #define LU_STRLEN 32
@@ -34,6 +35,39 @@ static inline int lu_str32_cmp(lu_str32 *self, lu_str32 *other) {
 
 static inline int lu_str32_cmpmem(lu_str32 *self, const char *other) {
     return strncmp(self->str, other, LU_STRLEN);
+}
+
+static inline uint32_t lu_hash_rotleft(uint32_t v, uint8_t bits) {
+    return (v << bits) | (v >> (32 - bits));
+}
+
+static inline uint32_t lu_str32_hash(lu_str32 *self, uint32_t seed) {
+    static const uint32_t PRIMES[4] = {2654435761U, 2246822519U, 3266489917U};
+    uint32_t state[4] = {
+        seed + PRIMES[0] + PRIMES[1],
+        seed + PRIMES[1],
+        seed,
+        seed - PRIMES[0]
+    };
+
+    const uint32_t *block = (const uint32_t*)self->str;
+    state[0] = lu_hash_rotleft(state[0] + block[0] * PRIMES[1], 13) * PRIMES[0];
+    state[1] = lu_hash_rotleft(state[1] + block[1] * PRIMES[1], 13) * PRIMES[0];
+    state[2] = lu_hash_rotleft(state[2] + block[2] * PRIMES[1], 13) * PRIMES[0];
+    state[3] = lu_hash_rotleft(state[3] + block[3] * PRIMES[1], 13) * PRIMES[0];
+
+    state[0] = lu_hash_rotleft(state[0] + block[4] * PRIMES[1], 13) * PRIMES[0];
+    state[1] = lu_hash_rotleft(state[1] + block[5] * PRIMES[1], 13) * PRIMES[0];
+    state[2] = lu_hash_rotleft(state[2] + block[6] * PRIMES[1], 13) * PRIMES[0];
+    state[3] = lu_hash_rotleft(state[3] + block[7] * PRIMES[1], 13) * PRIMES[0];
+
+    uint32_t result = lu_hash_rotleft(state[0], 1) + lu_hash_rotleft(state[1], 7) + lu_hash_rotleft(state[2], 12) + lu_hash_rotleft(state[3], 18);
+    result ^= result >> 15;
+    result *= PRIMES[1];
+    result ^= result >> 13;
+    result *= PRIMES[2];
+    result ^= result >> 16;
+    return result;
 }
 
 #endif // __LU_FIXED_STRING_H__
--- test.c
+++ test.c
@@ -1,4 +1,4 @@
-#include "luc_fixedstr.h"
+#include "lu_fixedstr.h"
 #include <stdio.h>
 
 int main(void) {
@@ -13,6 +13,21 @@ int main(void) {
     printf("Equal: %s\n", !cmpres ? "Equal" : "Not equal");
     lu_str32_clear(&str);
     printf("Empty: %s\n", str.str);
+
+    str = lu_str32_from_literal("1");
+    printf("Hash (%s): %u\n", str.str, lu_str32_hash(&str, 0));
+
+    str = lu_str32_from_literal("2");
+    printf("Hash (%s): %u\n", str.str, lu_str32_hash(&str, 0));
+
+    str = lu_str32_from_literal("HOLAHOLA");
+    printf("Hash (%s): %u\n", str.str, lu_str32_hash(&str, 0));
+
+    str = lu_str32_from_literal("_LUCAS_");
+    printf("Hash (%s): %u\n", str.str, lu_str32_hash(&str, 0));
+
+    str = lu_str32_from_literal("JejeProbandoHolamigo");
+    printf("Hash (%s): %u\n", str.str, lu_str32_hash(&str, 0));
 
     return 0;
 }
