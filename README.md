03f66436a0b344069f45dea056c4360f60bc7813
Lucas Merenciano <lucasmermar@gmail.com>
2025-03-04 08:15:14 +0100

    buffer wip

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 Collection of utilities written in C99.
 
 ##TODO
-- Lock-free ring buffer
 - Memory mapping
 - Simple math (vec and lerp)
 - Dynamic array
 - Hash map
 - Pool
+- Lock-free ring buffer
--- include/llulu/lu_buffer.h
+++ include/llulu/lu_buffer.h
@@ -2,10 +2,47 @@
 #define __LLULU_BUFFER_H__
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef struct lu_data {
+    const void *data;
+    size_t size; /* Bytes */
+} lu_data;
 
 typedef struct lu_buffer {
-    char *buf;
+    char *data;
     size_t size;
 } lu_buffer;
+
+typedef struct lu_array {
+    lu_buffer buf;
+    size_t count;
+    size_t elem_sz;
+} lu_array;
+
+void *lu_array_push(lu_array *self, int count, const void *data)
+{
+    if ((self->count + count) * self->elem_sz > self->buf.size) {
+        void *new_data = malloc(self->buf.size * 2);
+        if (new_data) {
+            memcpy(new_data, self->buf.data, self->buf.size);
+            free(self->buf.data);
+            self->buf.data = new_data;
+            self->buf.size *= 2;
+        } else {
+            printf("lu_arr_push failed to realloc buffer to %llu bytes.\n", self->buf.size * 2);
+            return NULL;
+        }
+    }
+
+    void *new_value = &self->buf.data[self->count * self->elem_sz];
+    memcpy(new_value, data, self->elem_sz * count);
+    self->count += count;
+    return new_value;
+}
+
+
 
 #endif /* __LLULU_BUFFER_H__ */
